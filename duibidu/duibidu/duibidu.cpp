b5b9231b36317b6e11512719d0893e8f65cebb8a
jile-2-28 <1611484513@qq.com>
2020-02-02 14:17:47 +0800

    contrast and bright

---
+++ duibidu/duibidu/duibidu.cpp
@@ -0,0 +1,71 @@
+﻿// duibidu.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+static void on_ContrastAndBright(int, void*);
+
+int g_nContrastValue;
+int g_nBrightValue;
+Mat g_srcImage, g_dstImage;
+
+int main()
+{
+    g_srcImage = imread("1.jpg");
+    if (!g_srcImage.data) { printf("读取图片错误，请确定目录下是否有imread函数指定图片存在~!"); return false; }
+    g_dstImage = Mat::zeros(g_srcImage.size(), g_srcImage.type());
+
+    g_nContrastValue = 80;
+    g_nBrightValue = 80;
+    namedWindow("【效果图窗口】", 1);
+
+	//创建轨迹条
+	createTrackbar("对比度：", "【效果图窗口】", &g_nContrastValue, 300, on_ContrastAndBright);
+	createTrackbar("亮   度：", "【效果图窗口】", &g_nBrightValue, 200, on_ContrastAndBright);
+
+	//调用回调函数
+	on_ContrastAndBright(g_nContrastValue, 0);
+	on_ContrastAndBright(g_nBrightValue, 0);
+
+	//按下“q”键时，程序退出
+	while (char(waitKey(1)) != 'q') {}
+	return 0;
+}
+
+static void on_ContrastAndBright(int, void*)
+{
+
+	// 创建窗口
+	namedWindow("【原始图窗口】", 1);
+
+	// 三个for循环，执行运算 g_dstImage(i,j) = a*g_srcImage(i,j) + b
+	for (int y = 0; y < g_srcImage.rows; y++)
+	{
+		for (int x = 0; x < g_srcImage.cols; x++)
+		{
+			for (int c = 0; c < 3; c++)
+			{
+				g_dstImage.at<Vec3b>(y, x)[c] = saturate_cast<uchar>((g_nContrastValue * 0.01) * (g_srcImage.at<Vec3b>(y, x)[c]) + g_nBrightValue);
+			}
+		}
+	}
+
+	// 显示图像
+	imshow("【原始图窗口】", g_srcImage);
+	imshow("【效果图窗口】", g_dstImage);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
