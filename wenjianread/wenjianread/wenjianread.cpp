2fe288d55efe7ff6571937ca36a174e9eb06ad11
jile-2-28 <1611484513@qq.com>
2020-02-02 22:47:02 +0800

    xml和yaml文件的读取

---
+++ wenjianread/wenjianread/wenjianread.cpp
@@ -0,0 +1,69 @@
+﻿// wenjianread.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "opencv2/opencv.hpp"  
+#include <time.h> 
+#include <iostream>    
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	//改变console字体颜色
+	system("color 6F");
+
+	//初始化
+	FileStorage fs2("test.yaml", FileStorage::READ);
+
+	// 第一种方法，对FileNode操作
+	int frameCount = (int)fs2["frameCount"];
+
+	std::string date;
+	// 第二种方法，使用FileNode运算符> > 
+	fs2["calibrationDate"] >> date;
+
+	Mat cameraMatrix2, distCoeffs2;
+	fs2["cameraMatrix"] >> cameraMatrix2;
+	fs2["distCoeffs"] >> distCoeffs2;
+
+	cout << "frameCount: " << frameCount << endl
+		<< "calibration date: " << date << endl
+		<< "camera matrix: " << cameraMatrix2 << endl
+		<< "distortion coeffs: " << distCoeffs2 << endl;
+
+	FileNode features = fs2["features"];
+	FileNodeIterator it = features.begin(), it_end = features.end();
+	int idx = 0;
+	std::vector<uchar> lbpval;
+
+	//使用FileNodeIterator遍历序列
+	for (; it != it_end; ++it, idx++)
+	{
+		cout << "feature #" << idx << ": ";
+		cout << "x=" << (int)(*it)["x"] << ", y=" << (int)(*it)["y"] << ", lbp: (";
+		// 我们也可以使用使用filenode > > std::vector操作符很容易的读数值阵列
+		(*it)["lbp"] >> lbpval;
+		for (int i = 0; i < (int)lbpval.size(); i++)
+			cout << " " << (int)lbpval[i];
+		cout << ")" << endl;
+	}
+	fs2.release();
+
+	//程序结束，输出一些帮助文字
+	printf("\n文件读取完毕，请输入任意键结束程序~");
+	getchar();
+
+	return 0;
+}
+
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
