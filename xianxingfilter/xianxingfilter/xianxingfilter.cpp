a01add035ec25eaf5adf4f91a6413aa2ca0f1ddb
jile-2-28 <1611484513@qq.com>
2020-02-03 20:56:26 +0800

    xianxingfilter

---
+++ xianxingfilter/xianxingfilter/xianxingfilter.cpp
@@ -0,0 +1,102 @@
+﻿// xianxingfilter.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+Mat g_srcImage, g_dstImage1, g_dstImage2, g_dstImage3;//存储图片的Mat类型
+int g_nBoxFilterValue = 3;  //方框滤波参数值
+int g_nMeanBlurValue = 3;  //均值滤波参数值
+int g_nGaussianBlurValue = 3;  //高斯滤波参数值
+
+static void on_BoxFilter(int, void*);		//均值滤波
+static void on_MeanBlur(int, void*);		//均值滤波
+static void on_GaussianBlur(int, void*);			//高斯滤波
+
+int main()
+{
+	//改变console字体颜色
+	system("color 5F");
+
+	// 载入原图
+	g_srcImage = imread("2.jpg", 1);
+	if (!g_srcImage.data) { printf("Oh，no，读取srcImage错误~！ \n"); return false; }
+
+	g_dstImage1 = g_srcImage.clone();
+	g_dstImage2 = g_srcImage.clone();
+	g_dstImage3 = g_srcImage.clone();
+
+	//显示原图
+	namedWindow("【<0>原图窗口】", 1);
+	imshow("【<0>原图窗口】", g_srcImage);
+
+    //创建窗口
+	namedWindow("【<1>方框滤波】", 1);
+	//创建轨迹条
+	createTrackbar("内核值：", "【<1>方框滤波】", &g_nBoxFilterValue, 40, on_BoxFilter);
+	on_BoxFilter(g_nBoxFilterValue, 0);
+	imshow("【<1>方框滤波】", g_dstImage1);
+
+	//创建窗口
+	namedWindow("【<2>均值滤波】", 1);
+	//创建轨迹条
+	createTrackbar("内核值：", "【<2>均值滤波】", &g_nMeanBlurValue, 40, on_MeanBlur);
+	on_MeanBlur(g_nMeanBlurValue, 0);
+
+	//创建窗口
+	namedWindow("【<3>高斯滤波】", 1);
+	//创建轨迹条
+	createTrackbar("内核值：", "【<3>高斯滤波】", &g_nGaussianBlurValue, 40, on_GaussianBlur);
+	on_GaussianBlur(g_nGaussianBlurValue, 0);
+
+	//输出一些帮助信息
+	cout << endl << "\t运行成功，请调整滚动条观察图像效果~\n\n"
+		<< "\t按下“q”键时，程序退出。\n";
+
+	//按下“q”键时，程序退出
+	while (char(waitKey(1)) != 'q') {}
+
+	return 0;
+}
+
+static void on_BoxFilter(int, void*)
+{
+	//方框滤波操作
+	boxFilter(g_srcImage, g_dstImage1, -1, Size(g_nBoxFilterValue + 1, g_nBoxFilterValue + 1));
+	//显示窗口
+	imshow("【<1>方框滤波】", g_dstImage1);
+}
+
+static void on_MeanBlur(int, void*)
+{
+	//均值滤波操作
+	blur(g_srcImage, g_dstImage2, Size(g_nMeanBlurValue + 1, g_nMeanBlurValue + 1), Point(-1, -1));
+	//显示窗口
+	imshow("【<2>均值滤波】", g_dstImage2);
+}
+
+static void on_GaussianBlur(int, void*)
+{
+	//高斯滤波操作
+	GaussianBlur(g_srcImage, g_dstImage3, Size(g_nGaussianBlurValue * 2 + 1, g_nGaussianBlurValue * 2 + 1), 0, 0);
+	//显示窗口
+	imshow("【<3>高斯滤波】", g_dstImage3);
+}
+
+
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
