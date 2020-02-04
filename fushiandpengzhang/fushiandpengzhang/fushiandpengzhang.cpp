ab255c20fc6b8e2ec2883e7a321ee769eafcbf5e
jile-2-28 <1611484513@qq.com>
2020-02-04 21:51:13 +0800

    dilate  and erode

---
+++ fushiandpengzhang/fushiandpengzhang/fushiandpengzhang.cpp
@@ -0,0 +1,91 @@
+﻿// fushiandpengzhang.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+using namespace std;
+using namespace cv;
+
+Mat g_srcImage, g_dstImage;//原始图和效果图
+int g_nTrackbarNumer = 0;//0表示腐蚀erode, 1表示膨胀dilate
+int g_nStructElementSize = 3; //结构元素(内核矩阵)的尺寸
+
+void Process();//膨胀和腐蚀的处理函数
+void on_TrackbarNumChange(int, void*);//回调函数
+void on_ElementSizeChange(int, void*);//回调函数
+
+int main()
+{
+	//改变console字体颜色
+	system("color 2F");
+
+	//载入原图
+	g_srcImage = imread("1.jpg");
+	if (!g_srcImage.data) { printf("读取srcImage错误~！ \n"); return false; }
+
+	//显示原始图
+	namedWindow("【原始图】");
+	imshow("【原始图】", g_srcImage);
+
+	//进行初次腐蚀操作并显示效果图
+	namedWindow("【效果图】");
+	//获取自定义核
+	Mat element = getStructuringElement(MORPH_RECT, Size(2 * g_nStructElementSize + 1, 2 * g_nStructElementSize + 1), Point(g_nStructElementSize, g_nStructElementSize));
+	erode(g_srcImage, g_dstImage, element);
+	imshow("【效果图】", g_dstImage);
+
+	//创建轨迹条
+	createTrackbar("腐蚀/膨胀", "【效果图】", &g_nTrackbarNumer, 1, on_TrackbarNumChange);
+	createTrackbar("内核尺寸", "【效果图】", &g_nStructElementSize, 21, on_ElementSizeChange);
+
+	//输出一些帮助信息
+	cout << endl << "\t运行成功，请调整滚动条观察图像效果~\n\n"
+		<< "\t按下“q”键时，程序退出。\n";
+
+	//轮询获取按键信息，若下q键，程序退出
+	while (char(waitKey(1)) != 'q') {}
+
+	return 0;
+}
+
+void Process()
+{
+	//获取自定义核
+	Mat element = getStructuringElement(MORPH_RECT, Size(2 * g_nStructElementSize + 1, 2 * g_nStructElementSize + 1), Point(g_nStructElementSize, g_nStructElementSize));
+
+	//进行腐蚀或膨胀操作
+	if (g_nTrackbarNumer == 0) {
+		erode(g_srcImage, g_dstImage, element);
+	}
+	else {
+		dilate(g_srcImage, g_dstImage, element);
+	}
+
+	//显示效果图
+	imshow("【效果图】", g_dstImage);
+}
+
+
+void on_TrackbarNumChange(int, void*)
+{
+	//腐蚀和膨胀之间效果已经切换，回调函数体内需调用一次Process函数，使改变后的效果立即生效并显示出来
+	Process();
+}
+
+void on_ElementSizeChange(int, void*)
+{
+	//内核尺寸已改变，回调函数体内需调用一次Process函数，使改变后的效果立即生效并显示出来
+	Process();
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
