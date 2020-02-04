d337a5831fb11a902024c2519585ab05eb438a89
jile-2-28 <1611484513@qq.com>
2020-02-04 20:33:20 +0800

    image filter

---
+++ zonghefilter/zonghefilter/zonghefilter.cpp
@@ -0,0 +1,123 @@
+﻿// zonghefilter.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+Mat g_srcImage, g_dstImage1, g_dstImage2, g_dstImage3, g_dstImage4, g_dstImage5;
+int g_nBoxFilterValue = 6;  //方框滤波内核值
+int g_nMeanBlurValue = 10;  //均值滤波内核值
+int g_nGaussianBlurValue = 6;  //高斯滤波内核值
+int g_nMedianBlurValue = 10;  //中值滤波参数值
+int g_nBilateralFilterValue = 10;  //双边滤波参数值
+
+static void on_BoxFilter(int, void*);		//方框滤波
+static void on_MeanBlur(int, void*);		//均值块滤波器
+static void on_GaussianBlur(int, void*);			//高斯滤波器
+static void on_MedianBlur(int, void*);			//中值滤波器
+static void on_BilateralFilter(int, void*);			//双边滤波器
+
+int main()
+{
+	system("color 4F");
+
+	// 载入原图
+	g_srcImage = imread("1.jpg", 1);
+	if (!g_srcImage.data) { printf("读取srcImage错误~！ \n"); return false; }
+
+	//克隆原图到四个Mat类型中
+	g_dstImage1 = g_srcImage.clone();
+	g_dstImage2 = g_srcImage.clone();
+	g_dstImage3 = g_srcImage.clone();
+	g_dstImage4 = g_srcImage.clone();
+	g_dstImage5 = g_srcImage.clone();
+
+	//显示原图
+	namedWindow("【<0>原图窗口】", 1);
+	imshow("【<0>原图窗口】", g_srcImage);
+
+    //创建窗口
+	namedWindow("【<1>方框滤波】", 1);
+	//创建轨迹条
+	createTrackbar("内核值：", "【<1>方框滤波】", &g_nBoxFilterValue, 50, on_BoxFilter);
+	on_BoxFilter(g_nBoxFilterValue, 0);
+	imshow("【<1>方框滤波】", g_dstImage1);
+
+	//创建窗口
+	namedWindow("【<2>均值滤波】", 1);
+	//创建轨迹条
+	createTrackbar("内核值：", "【<2>均值滤波】", &g_nMeanBlurValue, 50, on_MeanBlur);
+	on_MeanBlur(g_nMeanBlurValue, 0);
+
+	//创建窗口
+	namedWindow("【<3>高斯滤波】", 1);
+	//创建轨迹条
+	createTrackbar("内核值：", "【<3>高斯滤波】", &g_nGaussianBlurValue, 50, on_GaussianBlur);
+	on_GaussianBlur(g_nGaussianBlurValue, 0);
+
+	//创建窗口
+	namedWindow("【<4>中值滤波】", 1);
+	//创建轨迹条
+	createTrackbar("参数值：", "【<4>中值滤波】", &g_nMedianBlurValue, 50, on_MedianBlur);
+	on_MedianBlur(g_nMedianBlurValue, 0);
+
+	//创建窗口
+	namedWindow("【<5>双边滤波】", 1);
+	//创建轨迹条
+	createTrackbar("参数值：", "【<5>双边滤波】", &g_nBilateralFilterValue, 50, on_BilateralFilter);
+	on_BilateralFilter(g_nBilateralFilterValue, 0);
+
+	cout << endl << "\t运行成功，请调整滚动条观察图像效果~\n\n"
+		<< "\t按下“q”键时，程序退出。\n";
+	while (char(waitKey(1)) != 'q') {}
+
+	return 0;
+}
+
+static void on_BoxFilter(int, void*)
+{
+	//方框滤波操作
+	boxFilter(g_srcImage, g_dstImage1, -1, Size(g_nBoxFilterValue + 1, g_nBoxFilterValue + 1));
+	//显示窗口
+	imshow("【<1>方框滤波】", g_dstImage1);
+}
+
+static void on_MeanBlur(int, void*)
+{
+	blur(g_srcImage, g_dstImage2, Size(g_nMeanBlurValue + 1, g_nMeanBlurValue + 1), Point(-1, -1));
+	imshow("【<2>均值滤波】", g_dstImage2);
+
+}
+
+static void on_GaussianBlur(int, void*)
+{
+	GaussianBlur(g_srcImage, g_dstImage3, Size(g_nGaussianBlurValue * 2 + 1, g_nGaussianBlurValue * 2 + 1), 0, 0);
+	imshow("【<3>高斯滤波】", g_dstImage3);
+}
+
+static void on_MedianBlur(int, void*)
+{
+	medianBlur(g_srcImage, g_dstImage4, g_nMedianBlurValue * 2 + 1);
+	imshow("【<4>中值滤波】", g_dstImage4);
+}
+
+static void on_BilateralFilter(int, void*)
+{
+	bilateralFilter(g_srcImage, g_dstImage5, g_nBilateralFilterValue, g_nBilateralFilterValue * 2, g_nBilateralFilterValue / 2);
+	imshow("【<5>双边滤波】", g_dstImage5);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
