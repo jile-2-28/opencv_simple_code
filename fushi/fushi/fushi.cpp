6bab78b46bf9a74f5a5dffa0073623d5f5de864f
jile-2-28 <1611484513@qq.com>
2020-02-04 21:16:17 +0800

    fushi and pengzhang

---
+++ fushi/fushi/fushi.cpp
@@ -0,0 +1,35 @@
+﻿// fushi.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+using namespace cv;
+
+int main()
+{
+	//载入原图  
+	Mat srcImage = imread("1.jpg");
+	//显示原图
+	imshow("【原图】腐蚀操作", srcImage);
+	//进行腐蚀操作 
+	Mat element = getStructuringElement(MORPH_RECT, Size(15, 15));
+	Mat dstImage;
+	erode(srcImage, dstImage, element);
+	//显示效果图 
+	imshow("【效果图】腐蚀操作", dstImage);
+	waitKey(0);
+
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
