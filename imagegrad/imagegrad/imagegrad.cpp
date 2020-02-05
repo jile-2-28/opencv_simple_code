68c2cbd604907c6930cbd6381841b22e140b2fbc
jile-2-28 <1611484513@qq.com>
2020-02-05 13:39:48 +0800

    morphologyEx operator

---
+++ imagegrad/imagegrad/imagegrad.cpp
@@ -0,0 +1,41 @@
+﻿// imagegrad.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace cv;
+
+int main()
+{
+	//载入原始图   
+	Mat image = imread("1.jpg");  //工程目录下应该有一张名为1.jpg的素材图
+	//创建窗口   
+	namedWindow("【原始图】形态学梯度");
+	namedWindow("【效果图】形态学梯度");
+	//显示原始图  
+	imshow("【原始图】形态学梯度", image);
+	//定义核
+	Mat element = getStructuringElement(MORPH_RECT, Size(15, 15));
+	//进行形态学操作
+	morphologyEx(image, image, MORPH_GRADIENT, element);
+	//显示效果图  
+	imshow("【效果图】形态学梯度", image);
+
+	waitKey(0);
+
+	return 0;
+}
+
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
