43c9bd5b983efe52c399e967e96e755f09673b8c
jile-2-28 <1611484513@qq.com>
2020-02-07 14:28:54 +0800

    suofang operator

---
+++ suofangzonghe/suofangzonghe/suofangzonghe.cpp
@@ -0,0 +1,128 @@
+﻿// suofangzonghe.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace std;
+using namespace cv;
+
+#define WINDOW_NAME "【程序窗口】"
+
+static void ShowHelpText();
+
+Mat g_srcImage, g_dstImage, g_tmpImage;
+
+int main()
+{
+	//改变console字体颜色
+	system("color 2F");
+
+	//显示帮助文字
+	ShowHelpText();
+
+	//载入原图
+	g_srcImage = imread("2.jpg");//工程目录下需要有一张名为2.jpg的测试图像，且其尺寸需被2的N次方整除，N为可以缩放的次数
+	if (!g_srcImage.data) { printf("Oh，no，读取srcImage错误~！ \n"); return false; }
+
+	// 创建显示窗口
+	namedWindow(WINDOW_NAME, WINDOW_AUTOSIZE);
+	imshow(WINDOW_NAME, g_srcImage);
+
+	//参数赋值
+	g_tmpImage = g_srcImage;
+	g_dstImage = g_tmpImage;
+
+	int key = 0;
+
+	//轮询获取按键信息
+	while (1)
+	{
+		key = waitKey(9);//读取键值到key变量中
+
+		//根据key变量的值，进行不同的操作
+		switch (key)
+		{  
+		case 27://按键ESC
+			return 0;
+			break;
+
+		case 'q'://按键Q
+			return 0;
+			break;
+ 
+		case 'a'://按键A按下，调用pyrUp函数
+			pyrUp(g_tmpImage, g_dstImage, Size(g_tmpImage.cols * 2, g_tmpImage.rows * 2));
+			printf(">检测到按键【A】被按下，开始进行基于【pyrUp】函数的图片放大：图片尺寸×2 \n");
+			break;
+
+		case 'w'://按键W按下，调用resize函数
+			resize(g_tmpImage, g_dstImage, Size(g_tmpImage.cols * 2, g_tmpImage.rows * 2));
+			printf(">检测到按键【W】被按下，开始进行基于【resize】函数的图片放大：图片尺寸×2 \n");
+			break;
+
+		case '1'://按键1按下，调用resize函数
+			resize(g_tmpImage, g_dstImage, Size(g_tmpImage.cols * 2, g_tmpImage.rows * 2));
+			printf(">检测到按键【1】被按下，开始进行基于【resize】函数的图片放大：图片尺寸×2 \n");
+			break;
+
+		case '3': //按键3按下，调用pyrUp函数
+			pyrUp(g_tmpImage, g_dstImage, Size(g_tmpImage.cols * 2, g_tmpImage.rows * 2));
+			printf(">检测到按键【3】被按下，开始进行基于【pyrUp】函数的图片放大：图片尺寸×2 \n");
+			break;
+
+		case 'd': //按键D按下，调用pyrDown函数
+			pyrDown(g_tmpImage, g_dstImage, Size(g_tmpImage.cols / 2, g_tmpImage.rows / 2));
+			printf(">检测到按键【D】被按下，开始进行基于【pyrDown】函数的图片缩小：图片尺寸/2\n");
+			break;
+
+		case  's': //按键S按下，调用resize函数
+			resize(g_tmpImage, g_dstImage, Size(g_tmpImage.cols / 2, g_tmpImage.rows / 2));
+			printf(">检测到按键【S】被按下，开始进行基于【resize】函数的图片缩小：图片尺寸/2\n");
+			break;
+
+		case '2'://按键2按下，调用resize函数
+			resize(g_tmpImage, g_dstImage, Size(g_tmpImage.cols / 2, g_tmpImage.rows / 2));
+			printf(">检测到按键【2】被按下，开始进行基于【resize】函数的图片缩小：图片尺寸/2\n");
+			break;
+
+		case '4': //按键4按下，调用pyrDown函数
+			pyrDown(g_tmpImage, g_dstImage, Size(g_tmpImage.cols / 2, g_tmpImage.rows / 2));
+			printf(">检测到按键【4】被按下，开始进行基于【pyrDown】函数的图片缩小：图片尺寸/2\n");
+			break;
+		}
+
+		//经过操作后，显示变化后的图
+		imshow(WINDOW_NAME, g_dstImage);
+
+		//将g_dstImage赋给g_tmpImage，方便下一次循环
+		g_tmpImage = g_dstImage;
+	}
+
+	return 0;
+
+}
+
+static void ShowHelpText()
+{
+	//输出一些帮助信息
+	printf("\n\t欢迎来到OpenCV图像金字塔和resize示例程序~\n\n");
+	printf("\n\n\t按键操作说明: \n\n"
+		"\t\t键盘按键【ESC】或者【Q】- 退出程序\n"
+		"\t\t键盘按键【1】或者【W】- 进行基于【resize】函数的图片放大\n"
+		"\t\t键盘按键【2】或者【S】- 进行基于【resize】函数的图片缩小\n"
+		"\t\t键盘按键【3】或者【A】- 进行基于【pyrUp】函数的图片放大\n"
+		"\t\t键盘按键【4】或者【D】- 进行基于【pyrDown】函数的图片缩小\n"
+	);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
