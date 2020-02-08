6f425326779b6c2f169747faaef6c6fa39c54a84
jile-2-28 <1611484513@qq.com>
2020-02-08 20:53:10 +0800

    sobel test

---
+++ sobeltest/sobeltest/sobeltest.cpp
@@ -0,0 +1,50 @@
+﻿// sobeltest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+
+int main()
+{
+	//创建 grad_x 和 grad_y 矩阵
+	Mat grad_x, grad_y;
+	Mat abs_grad_x, abs_grad_y, dst;
+
+	//载入原始图  
+	Mat src = imread("lee.jpg");
+
+	//显示原始图 
+	imshow("【原始图】sobel边缘检测", src);
+
+	//求 X方向梯度
+	Sobel(src, grad_x, CV_16S, 1, 0, 3, 1, 1, BORDER_DEFAULT);
+	convertScaleAbs(grad_x, abs_grad_x);
+	imshow("【效果图】 X方向Sobel", abs_grad_x);
+
+	//求Y方向梯度
+	Sobel(src, grad_y, CV_16S, 0, 1, 3, 1, 1, BORDER_DEFAULT);
+	convertScaleAbs(grad_y, abs_grad_y);
+	imshow("【效果图】Y方向Sobel", abs_grad_y);
+
+	//合并梯度(近似)
+	addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, dst);
+	imshow("【效果图】整体方向Sobel", dst);
+
+	waitKey(0);
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
