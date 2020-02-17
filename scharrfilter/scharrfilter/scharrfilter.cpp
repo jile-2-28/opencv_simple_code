0b470d8db574f30237600583878a23b1b670c1b4
jile-2-28 <1611484513@qq.com>
2020-02-17 11:15:50 +0800

    scharr

---
+++ scharrfilter/scharrfilter/scharrfilter.cpp
@@ -0,0 +1,39 @@
+﻿// scharrfilter.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+
+int main()
+{
+    Mat grad_x, grad_y;
+    Mat abs_grad_x, abs_grad_y, dst;
+    Mat src = imread("jay.jpg");
+    imshow("【原始图】", src);
+    Scharr(src, grad_x, CV_16S, 1, 0, 1, 0, BORDER_DEFAULT);
+    convertScaleAbs(grad_x, abs_grad_x);
+    imshow("【效果图】x方向", abs_grad_x);
+    Scharr(src, grad_y, CV_16S, 0, 1, 1, 0, BORDER_DEFAULT);
+    convertScaleAbs(grad_y, abs_grad_y);
+    imshow("【效果图】y方向", abs_grad_y);
+
+    addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, dst);
+    imshow("【效果图】合并", dst);
+
+    waitKey(0);
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
