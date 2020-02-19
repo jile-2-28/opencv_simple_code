c608713693b92bb6eb28e11d6b70beddd113f0e6
jile-2-28 <1611484513@qq.com>
2020-02-19 14:31:28 +0800

    hough line

---
+++ houghxian/houghxian/houghxian.cpp
@@ -0,0 +1,61 @@
+﻿// houghxian.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    Mat srcImage = imread("jay.jpg");
+    Mat midImage, dstImage;
+
+    Canny(srcImage, midImage, 50, 200, 3);
+    cvtColor(midImage, dstImage,COLOR_GRAY2BGR);
+
+    vector<Vec2f> lines;//定义一个矢量结构lines用于存放得到的线段矢量集合
+    HoughLines(midImage, lines, 1, CV_PI / 180, 150, 0, 0);
+
+	//依次在图中绘制出每条线段
+	for (size_t i = 0; i < lines.size(); i++)
+	{
+		float rho = lines[i][0], theta = lines[i][1];
+		Point pt1, pt2;
+		double a = cos(theta), b = sin(theta);
+		double x0 = a * rho, y0 = b * rho;
+		pt1.x = cvRound(x0 + 1000 * (-b));//返回跟参数最接近的整数值
+		pt1.y = cvRound(y0 + 1000 * (a));
+		pt2.x = cvRound(x0 - 1000 * (-b));
+		pt2.y = cvRound(y0 - 1000 * (a));
+		line(dstImage, pt1, pt2, Scalar(55, 100, 195), 1, LINE_AA);
+	}
+
+	//显示原始图  
+	imshow("【原始图】", srcImage);
+
+	//边缘检测后的图 
+	imshow("【边缘检测后的图】", midImage);
+
+	//显示效果图  
+	imshow("【效果图】", dstImage);
+
+	waitKey(0);
+
+	return 0;
+
+
+}
+
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
