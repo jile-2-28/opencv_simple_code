b98bd30ec25ef4e808dd06e87edf4ac2ba0ef0e0
jile-2-28 <1611484513@qq.com>
2020-02-19 14:47:48 +0800

    houghlinesp

---
+++ houghgailv/houghgailv/houghgailv.cpp
@@ -0,0 +1,57 @@
+﻿// houghgailv.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	//载入原始图和Mat变量定义   
+	Mat srcImage = imread("1.jpg");  //工程目录下应该有一张名为1.jpg的素材图
+	Mat midImage, dstImage;//临时变量和目标图的定义
+
+	//进行边缘检测和转化为灰度图
+	Canny(srcImage, midImage, 50, 200, 3);//进行一此canny边缘检测
+	cvtColor(midImage, dstImage, COLOR_GRAY2BGR);//转化边缘检测后的图为灰度图
+
+	//进行霍夫线变换
+	vector<Vec4i> lines;//定义一个矢量结构lines用于存放得到的线段矢量集合
+	HoughLinesP(midImage, lines, 1, CV_PI / 180, 80, 50, 10);
+
+	//依次在图中绘制出每条线段
+	for (size_t i = 0; i < lines.size(); i++)
+	{
+		Vec4i l = lines[i];
+		line(dstImage, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(186, 88, 255), 1, LINE_AA);
+	}
+
+	//显示原始图  
+	imshow("【原始图】", srcImage);
+
+	//边缘检测后的图 
+	imshow("【边缘检测后的图】", midImage);
+
+	//显示效果图  
+	imshow("【效果图】", dstImage);
+
+	waitKey(0);
+
+	return 0;
+}
+
+
+
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
