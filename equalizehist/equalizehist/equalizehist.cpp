ecb77f2f264a2231575d9bbf7395aab5c5e3ebed
jile-2-28 <1611484513@qq.com>
2020-02-21 15:42:04 +0800

    equalizehist

---
+++ equalizehist/equalizehist/equalizehist.cpp
@@ -0,0 +1,41 @@
+﻿// equalizehist.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+using namespace cv;
+
+int main()
+{
+	// 加载源图像
+	Mat srcImage, dstImage;
+	srcImage = imread("1.jpg", 1);
+	if (!srcImage.data) { printf("读取图片错误，请确定目录下是否有imread函数指定图片存在~！ \n"); return false; }
+
+	// 转为灰度图并显示出来
+	cvtColor(srcImage, srcImage, COLOR_BGR2GRAY);
+	imshow("原始图", srcImage);
+
+	// 进行直方图均衡化
+	equalizeHist(srcImage, dstImage);
+
+	// 显示结果
+	imshow("经过直方图均衡化后的图", dstImage);
+
+	// 等待用户按键退出程序
+	waitKey(0);
+	return 0;
+
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
