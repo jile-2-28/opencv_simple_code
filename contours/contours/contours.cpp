a74a099b49408090889e8414e1e4fc6812b1ad90
jile-2-28 <1611484513@qq.com>
2020-02-22 16:12:54 +0800

    find contours

---
+++ contours/contours/contours.cpp
@@ -0,0 +1,49 @@
+﻿// contours.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    Mat srcImage = imread("1.jpg", 0);
+    imshow("原始图", srcImage);
+
+    Mat dstImage = Mat::zeros(srcImage.rows, srcImage.cols, CV_8UC3);
+
+    srcImage = srcImage > 119;
+    imshow("取阈值后的原始图", srcImage);
+
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+
+	findContours(srcImage, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
+
+	// 遍历所有顶层的轮廓， 以随机颜色绘制出每个连接组件颜色
+	int index = 0;
+	for (; index >= 0; index = hierarchy[index][0])
+	{
+		Scalar color(rand() & 255, rand() & 255, rand() & 255);
+		drawContours(dstImage, contours, index, color, FILLED, 8, hierarchy);
+	}
+
+	imshow("轮廓图", dstImage);
+
+	waitKey(0);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
