b8b0c11a932c2c597fa940facd9d6dcf05c43c57
jile-2-28 <1611484513@qq.com>
2020-02-23 15:43:20 +0800

    hull count

---
+++ Hull/Hull/Hull.cpp
@@ -0,0 +1,92 @@
+﻿// Hull.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+static void ShowHelpText()
+{
+	printf("\n\n\t\t\t   当前使用的OpenCV版本为：" CV_VERSION);
+	printf("\n\n  ----------------------------------------------------------------------------\n");
+	//输出一些帮助信息
+	printf("\n\t欢迎来到【凸包检测】示例程序~\n\n");
+	printf("\n\t按键操作说明: \n\n"
+		"\t\t键盘按键【ESC】、【Q】、【q】- 退出程序\n\n"
+		"\t\t键盘按键任意键 - 重新生成随机点，并进行凸包检测\n");
+}
+
+int main()
+{
+	//改变console字体颜色
+	system("color 1F");
+
+	//显示帮助文字
+	ShowHelpText();
+
+	//初始化变量和随机值
+	Mat image(600, 600, CV_8UC3);
+	RNG& rng = theRNG();//用其引用来接收theRNG函数返回的随机数生成器（RNG就是随机数生成器的类）
+
+	//循环，按下ESC,Q,q键程序退出，否则有键按下便一直更新
+	while (1)
+	{
+		//参数初始化
+		char key;//键值
+		int count = (unsigned)rng % 100 + 3;//随机生成点的数量
+		vector<Point> points; //点值
+
+		//随机生成点坐标
+		for (int i = 0; i < count; i++)
+		{
+			Point point;
+			point.x = rng.uniform(image.cols / 4, image.cols * 3 / 4);
+			point.y = rng.uniform(image.rows / 4, image.rows * 3 / 4);
+
+			points.push_back(point);
+		}
+
+		//检测凸包
+		vector<int> hull;
+		convexHull(Mat(points), hull, true);
+
+		//绘制出随机颜色的点
+		image = Scalar::all(0);
+		for (int i = 0; i < count; i++)
+			circle(image, points[i], 3, Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255)), FILLED, LINE_AA);
+
+		//准备参数
+		int hullcount = (int)hull.size();//凸包的边数
+		Point point0 = points[hull[hullcount - 1]];//连接凸包边的坐标点
+
+		//绘制凸包的边
+		for (int i = 0; i < hullcount; i++)
+		{
+			Point point = points[hull[i]];
+			line(image, point0, point, Scalar(255, 255, 255), 2, LINE_AA);
+			point0 = point;
+		}
+
+		//显示效果图
+		imshow("凸包检测示例", image);
+
+		//按下ESC,Q,或者q，程序退出
+		key = (char)waitKey();
+		if (key == 27 || key == 'q' || key == 'Q')
+			break;
+	}
+
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
