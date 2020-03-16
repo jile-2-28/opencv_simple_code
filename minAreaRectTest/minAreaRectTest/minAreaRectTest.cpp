b77f8031451c34deef0a9c001e1aeaa5db371dee
jile-2-28 <1611484513@qq.com>
2020-03-16 11:17:16 +0800

    minAreaRect

---
+++ minAreaRectTest/minAreaRectTest/minAreaRectTest.cpp
@@ -0,0 +1,59 @@
+﻿// minAreaRectTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    Mat image(600, 600, CV_8UC3);
+    RNG& rng = theRNG();
+
+    while (1)
+    {
+        int count = rng.uniform(3, 103);
+        vector<Point> points;
+        for (int i = 0; i < count; i++)
+        {
+            Point point;
+            point.x = rng.uniform(image.cols / 4, image.cols * 3 / 4);
+            point.y = rng.uniform(image.rows / 4, image.rows * 3 / 4);
+            points.push_back(point);
+        }
+        RotatedRect box = minAreaRect(Mat(points));
+        Point2f vertex[4];
+        box.points(vertex);
+
+        image = Scalar::all(0);
+        for (int i = 0; i < count; i++) 
+        {
+            circle(image, points[i], 3, Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255)), FILLED, LINE_AA);
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            line(image, vertex[i], vertex[(i + 1) % 4], Scalar(100, 200, 211), 2, LINE_AA);
+        }
+
+        imshow("矩形包围示例", image);
+
+        char key = (char)waitKey();
+        if(key == 27 || key == 'q' || key == 'Q')
+        break;
+
+    }
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
