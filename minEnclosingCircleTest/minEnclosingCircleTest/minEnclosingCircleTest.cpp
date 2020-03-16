8a91ab7cf796e962ccd119b20f17fef8760f1510
jile-2-28 <1611484513@qq.com>
2020-03-16 11:57:09 +0800

    minEnclosingCircleTest

---
+++ minEnclosingCircleTest/minEnclosingCircleTest/minEnclosingCircleTest.cpp
@@ -0,0 +1,85 @@
+﻿// minEnclosingCircleTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+using namespace cv;
+using namespace std;
+
+static void ShowHelpText()
+{
+	//输出一些帮助信息
+	printf("\n\n\t\t\t欢迎来到【寻找最小面积的包围圆】示例程序~\n");
+	printf("\n\n\t按键操作说明: \n\n"
+		"\t\t键盘按键【ESC】、【Q】、【q】- 退出程序\n\n"
+		"\t\t键盘按键任意键 - 重新生成随机点，并寻找最小面积的包围圆\n");
+}
+
+int main()
+{
+	//改变console字体颜色
+	system("color 1F");
+
+	//显示帮助文字
+	ShowHelpText();
+
+	//初始化变量和随机值
+	Mat image(600, 600, CV_8UC3);
+	RNG& rng = theRNG();
+
+	//循环，按下ESC,Q,q键程序退出，否则有键按下便一直更新
+	while (1)
+	{
+		//参数初始化
+		int count = rng.uniform(3, 103);//随机生成点的数量
+		vector<Point> points;//点值
+
+		//随机生成点坐标
+		for (int i = 0; i < count; i++)
+		{
+
+			Point point;
+			point.x = rng.uniform(image.cols / 4, image.cols * 3 / 4);
+			point.y = rng.uniform(image.rows / 4, image.rows * 3 / 4);
+
+			points.push_back(point);
+		}
+
+		//对给定的 2D 点集，寻找最小面积的包围圆
+		Point2f center;
+		float radius = 0;
+		minEnclosingCircle(Mat(points), center, radius);
+
+		//绘制出随机颜色的点
+		image = Scalar::all(0);
+		for (int i = 0; i < count; i++)
+			circle(image, points[i], 3, Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255)), FILLED, LINE_AA);
+
+
+		//绘制出最小面积的包围圆
+		circle(image, center, cvRound(radius), Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255)), 2, LINE_AA);
+
+		//显示窗口
+		imshow("圆形包围示例", image);
+
+		//按下ESC,Q,或者q，程序退出
+		char key = (char)waitKey();
+		if (key == 27 || key == 'q' || key == 'Q') // 'ESC'
+			break;
+	}
+
+	return 0;
+}
+
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
