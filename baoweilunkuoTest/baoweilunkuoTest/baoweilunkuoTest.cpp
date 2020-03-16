da966c766e1aac38e3999dd2e66e4c10696931e0
jile-2-28 <1611484513@qq.com>
2020-03-16 16:18:15 +0800

    contours test

---
+++ baoweilunkuoTest/baoweilunkuoTest/baoweilunkuoTest.cpp
@@ -0,0 +1,116 @@
+﻿// baoweilunkuoTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+using namespace cv;
+using namespace std;
+
+#define WINDOW_NAME1 "【原始图窗口】"        //为窗口标题定义的宏 
+#define WINDOW_NAME2 "【效果图窗口】"        //为窗口标题定义的宏 
+
+Mat g_srcImage;
+Mat g_grayImage;
+int g_nThresh = 50;//阈值
+int g_nMaxThresh = 255;//阈值最大值
+RNG g_rng(12345);//随机数生成器
+
+void on_ContoursChange(int, void*);
+static void ShowHelpText();
+
+int main()
+{
+	//改变console字体颜色
+	system("color 1F");
+
+	//显示欢迎和帮助文字
+	ShowHelpText();
+
+	//载入3通道的原图像
+	g_srcImage = imread("lakers.jpg", 1);
+	if (!g_srcImage.data) { printf("读取图片错误，请确定目录下是否有imread函数指定的图片存在~！ \n"); return false; }
+
+	//得到原图的灰度图像并进行平滑
+	cvtColor(g_srcImage, g_grayImage, COLOR_BGR2GRAY);
+	blur(g_grayImage, g_grayImage, Size(3, 3));
+
+	//创建原始图窗口并显示
+	namedWindow(WINDOW_NAME1, WINDOW_AUTOSIZE);
+	imshow(WINDOW_NAME1, g_srcImage);
+
+	//设置滚动条并调用一次回调函数
+	createTrackbar(" 阈值:", WINDOW_NAME1, &g_nThresh, g_nMaxThresh, on_ContoursChange);
+	on_ContoursChange(0, 0);
+
+	waitKey(0);
+
+	return(0);
+
+}
+
+void on_ContoursChange(int, void*)
+{
+	//定义一些参数
+	Mat threshold_output;
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+
+	// 使用Threshold检测边缘
+	threshold(g_grayImage, threshold_output, g_nThresh, 255, THRESH_BINARY);
+
+	// 找出轮廓
+	findContours(threshold_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+	// 多边形逼近轮廓 + 获取矩形和圆形边界框
+	vector<vector<Point> > contours_poly(contours.size());
+	vector<Rect> boundRect(contours.size());
+	vector<Point2f>center(contours.size());
+	vector<float>radius(contours.size());
+
+	//一个循环，遍历所有部分，进行本程序最核心的操作
+	for (unsigned int i = 0; i < contours.size(); i++)
+	{
+		approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);//用指定精度逼近多边形曲线 
+		boundRect[i] = boundingRect(Mat(contours_poly[i]));//计算点集的最外面（up-right）矩形边界
+		minEnclosingCircle(contours_poly[i], center[i], radius[i]);//对给定的 2D点集，寻找最小面积的包围圆形 
+	}
+
+	// 绘制多边形轮廓 + 包围的矩形框 + 圆形框
+	Mat drawing = Mat::zeros(threshold_output.size(), CV_8UC3);
+	for (int unsigned i = 0; i < contours.size(); i++)
+	{
+		Scalar color = Scalar(g_rng.uniform(0, 255), g_rng.uniform(0, 255), g_rng.uniform(0, 255));//随机设置颜色
+		drawContours(drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());//绘制轮廓
+		rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0);//绘制矩形
+		circle(drawing, center[i], (int)radius[i], color, 2, 8, 0);//绘制圆
+	}
+
+	// 显示效果图窗口
+	namedWindow(WINDOW_NAME2, WINDOW_AUTOSIZE);
+	imshow(WINDOW_NAME2, drawing);
+}
+
+static void ShowHelpText()
+{
+	//输出欢迎信息和OpenCV版本
+	printf("\n\n\t\t\t   当前使用的OpenCV版本为：" CV_VERSION);
+	printf("\n\n  ----------------------------------------------------------------------------\n");
+
+	//输出一些帮助信息  
+	printf("\n\n\n\t欢迎来到【创建包围轮廓的矩形和圆形边界框】示例程序~\n\n");
+	printf("\n\n\t按键操作说明: \n\n"
+		"\t\t键盘按键【ESC】- 退出程序\n\n"
+		"\t\t滑动滚动条 - 改变阈值\n\n");
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
