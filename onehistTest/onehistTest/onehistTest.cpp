70dc2412dbb00919daa1ddf8fc34646376b6d177
jile-2-28 <1611484513@qq.com>
2020-03-22 11:08:34 +0800

    onehist Test

---
+++ onehistTest/onehistTest/onehistTest.cpp
@@ -0,0 +1,66 @@
+﻿// onehistTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+void ShowHelpText()
+{
+	//输出OpenCV版本
+	printf("\n\n\t\t\t   当前使用的OpenCV版本为：" CV_VERSION);
+	printf("\n\n  ----------------------------------------------------------------------------\n");
+}
+
+int main()
+{
+	//载入原图并显示
+	Mat srcImage = imread("jay.jpg", 0);
+	imshow("原图", srcImage);
+	if (!srcImage.data) { cout << "fail to load image" << endl; 	return 0; }
+
+	system("color 1F");
+	ShowHelpText();
+
+	//定义变量
+	MatND dstHist;       // 在cv中用CvHistogram *hist = cvCreateHist
+	int dims = 1;
+	float hranges[] = { 0, 255 };
+	const float* ranges[] = { hranges };   // 这里需要为const类型
+	int size = 256;
+	int channels = 0;
+
+	//计算图像的直方图
+	calcHist(&srcImage, 1, &channels, Mat(), dstHist, dims, &size, ranges);    // cv 中是cvCalcHist
+	int scale = 1;
+
+	Mat dstImage(size * scale, size, CV_8U, Scalar(0));
+	//获取最大值和最小值
+	double minValue = 0;
+	double maxValue = 0;
+	minMaxLoc(dstHist, &minValue, &maxValue, 0, 0);  //  在cv中用的是cvGetMinMaxHistValue
+
+	//绘制出直方图
+	int hpt = saturate_cast<int>(0.9 * size);
+	for (int i = 0; i < 256; i++)
+	{
+		float binValue = dstHist.at<float>(i);           //   注意hist中是float类型    而在OpenCV1.0版中用cvQueryHistValue_1D
+		int realValue = saturate_cast<int>(binValue * hpt / maxValue);
+		rectangle(dstImage, Point(i * scale, size - 1), Point((i + 1) * scale - 1, size - realValue), Scalar(255));
+	}
+	imshow("一维直方图", dstImage);
+	waitKey(0);
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
