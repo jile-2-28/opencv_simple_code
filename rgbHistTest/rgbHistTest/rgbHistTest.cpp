99b0539a46d1b2a9bbc9b1aed94173930d09ae76
jile-2-28 <1611484513@qq.com>
2020-03-22 11:39:32 +0800

    rgbHist test

---
+++ rgbHistTest/rgbHistTest/rgbHistTest.cpp
@@ -0,0 +1,108 @@
+﻿// rgbHistTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>  
+#include <opencv2/imgproc/imgproc.hpp>  
+using namespace cv;
+
+void ShowHelpText()
+{
+	printf("\n\n\t\t\t   当前使用的OpenCV版本为：" CV_VERSION);
+	printf("\n\n  ----------------------------------------------------------------------------\n");
+}
+
+
+int main()
+{
+
+	//载入素材图并显示
+	Mat srcImage;
+	srcImage = imread("jay.jpg");
+	imshow("素材图", srcImage);
+
+	system("color 3F");
+	ShowHelpText();
+
+	//参数准备
+	int bins = 256;
+	int hist_size[] = { bins };
+	float range[] = { 0, 256 };
+	const float* ranges[] = { range };
+	MatND redHist, grayHist, blueHist;
+	int channels_r[] = { 0 };
+
+	//进行直方图的计算（红色分量部分）
+	calcHist(&srcImage, 1, channels_r, Mat(), //不使用掩膜
+		redHist, 1, hist_size, ranges,
+		true, false);
+
+	//进行直方图的计算（绿色分量部分）
+	int channels_g[] = { 1 };
+	calcHist(&srcImage, 1, channels_g, Mat(), // do not use mask
+		grayHist, 1, hist_size, ranges,
+		true, // the histogram is uniform
+		false);
+
+	//进行直方图的计算（蓝色分量部分）
+	int channels_b[] = { 2 };
+	calcHist(&srcImage, 1, channels_b, Mat(), // do not use mask
+		blueHist, 1, hist_size, ranges,
+		true, // the histogram is uniform
+		false);
+
+	//-----------------------绘制出三色直方图------------------------
+	//参数准备
+	double maxValue_red, maxValue_green, maxValue_blue;
+	minMaxLoc(redHist, 0, &maxValue_red, 0, 0);
+	minMaxLoc(grayHist, 0, &maxValue_green, 0, 0);
+	minMaxLoc(blueHist, 0, &maxValue_blue, 0, 0);
+	int scale = 1;
+	int histHeight = 256;
+	Mat histImage = Mat::zeros(histHeight, bins * 3, CV_8UC3);
+
+	//正式开始绘制
+	for (int i = 0; i < bins; i++)
+	{
+		//参数准备
+		float binValue_red = redHist.at<float>(i);
+		float binValue_green = grayHist.at<float>(i);
+		float binValue_blue = blueHist.at<float>(i);
+		int intensity_red = cvRound(binValue_red * histHeight / maxValue_red);  //要绘制的高度
+		int intensity_green = cvRound(binValue_green * histHeight / maxValue_green);  //要绘制的高度
+		int intensity_blue = cvRound(binValue_blue * histHeight / maxValue_blue);  //要绘制的高度
+
+		//绘制红色分量的直方图
+		rectangle(histImage, Point(i * scale, histHeight - 1),
+			Point((i + 1) * scale - 1, histHeight - intensity_red),
+			Scalar(255, 0, 0));
+
+		//绘制绿色分量的直方图
+		rectangle(histImage, Point((i + bins) * scale, histHeight - 1),
+			Point((i + bins + 1) * scale - 1, histHeight - intensity_green),
+			Scalar(0, 255, 0));
+
+		//绘制蓝色分量的直方图
+		rectangle(histImage, Point((i + bins * 2) * scale, histHeight - 1),
+			Point((i + bins * 2 + 1) * scale - 1, histHeight - intensity_blue),
+			Scalar(0, 0, 255));
+
+	}
+
+	//在窗口中显示出绘制好的直方图
+	imshow("图像的RGB直方图", histImage);
+	waitKey(0);
+	return 0;
+}
+
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
