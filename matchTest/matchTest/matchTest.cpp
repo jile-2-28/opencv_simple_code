c7231f99345835ad96424f5a47ac6d00b550837a
jile-2-28 <1611484513@qq.com>
2020-03-24 20:42:14 +0800

    match Test

---
+++ matchTest/matchTest/matchTest.cpp
@@ -0,0 +1,110 @@
+﻿// matchTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+using namespace cv;
+
+#define WINDOW_NAME1 "【原始图片】"        //为窗口标题定义的宏 
+#define WINDOW_NAME2 "【匹配窗口】"        //为窗口标题定义的宏 
+
+Mat g_srcImage; Mat g_templateImage; Mat g_resultImage;
+int g_nMatchMethod;
+int g_nMaxTrackbarNum = 5;
+
+void on_Matching(int, void*);
+static void ShowHelpText();
+
+int main()
+{
+	//改变console字体颜色
+	system("color 1F");
+
+	//显示帮助文字
+	ShowHelpText();
+
+	//载入原图像和模板块
+	g_srcImage = imread("1.jpg", 1);
+	g_templateImage = imread("2.jpg", 1);
+
+	//创建窗口
+	namedWindow(WINDOW_NAME1, WINDOW_AUTOSIZE);
+	namedWindow(WINDOW_NAME2, WINDOW_AUTOSIZE);
+
+	//创建滑动条并进行一次初始化
+	createTrackbar("方法", WINDOW_NAME1, &g_nMatchMethod, g_nMaxTrackbarNum, on_Matching);
+	on_Matching(0, 0);
+
+	waitKey(0);
+	return 0;
+
+}
+
+void on_Matching(int, void*)
+{
+	//给局部变量初始化
+	Mat srcImage;
+	g_srcImage.copyTo(srcImage);
+
+	//初始化用于结果输出的矩阵
+	int resultImage_cols = g_srcImage.cols - g_templateImage.cols + 1;
+	int resultImage_rows = g_srcImage.rows - g_templateImage.rows + 1;
+	g_resultImage.create(resultImage_cols, resultImage_rows, CV_32FC1);
+
+	//进行匹配和标准化
+	matchTemplate(g_srcImage, g_templateImage, g_resultImage, g_nMatchMethod);
+	normalize(g_resultImage, g_resultImage, 0, 1, NORM_MINMAX, -1, Mat());
+
+	//通过函数 minMaxLoc 定位最匹配的位置
+	double minValue; double maxValue; Point minLocation; Point maxLocation;
+	Point matchLocation;
+	minMaxLoc(g_resultImage, &minValue, &maxValue, &minLocation, &maxLocation, Mat());
+
+	//对于方法 SQDIFF 和 SQDIFF_NORMED, 越小的数值有着更高的匹配结果. 而其余的方法, 数值越大匹配效果越好
+	if (g_nMatchMethod == TM_SQDIFF || g_nMatchMethod == TM_SQDIFF_NORMED)
+	{
+		matchLocation = minLocation;
+	}
+	else
+	{
+		matchLocation = maxLocation;
+	}
+
+	//绘制出矩形，并显示最终结果
+	rectangle(srcImage, matchLocation, Point(matchLocation.x + g_templateImage.cols, matchLocation.y + g_templateImage.rows), Scalar(0, 0, 255), 2, 8, 0);
+	rectangle(g_resultImage, matchLocation, Point(matchLocation.x + g_templateImage.cols, matchLocation.y + g_templateImage.rows), Scalar(0, 0, 255), 2, 8, 0);
+
+	imshow(WINDOW_NAME1, srcImage);
+	imshow(WINDOW_NAME2, g_resultImage);
+
+}
+
+static void ShowHelpText()
+{
+	//输出欢迎信息和OpenCV版本
+	printf("\n\n\t\t\t   当前使用的OpenCV版本为：" CV_VERSION);
+	printf("\n\n  ----------------------------------------------------------------------------\n");
+	//输出一些帮助信息
+	printf("\t欢迎来到【模板匹配】示例程序~\n");
+	printf("\n\n\t请调整滑动条观察图像效果\n\n");
+	printf("\n\t滑动条对应的方法数值说明: \n\n"
+		"\t\t方法【0】- 平方差匹配法(SQDIFF)\n"
+		"\t\t方法【1】- 归一化平方差匹配法(SQDIFF NORMED)\n"
+		"\t\t方法【2】- 相关匹配法(TM CCORR)\n"
+		"\t\t方法【3】- 归一化相关匹配法(TM CCORR NORMED)\n"
+		"\t\t方法【4】- 相关系数匹配法(TM COEFF)\n"
+		"\t\t方法【5】- 归一化相关系数匹配法(TM COEFF NORMED)\n");
+}
+
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
